293876c47c96c2d6de3e6172deaf8a572976c49c
Vlad Loparev <loparev.official@yandex.ru>
2024-10-30 22:32:47 +0500

    add doxygen documentation + readme edit

---
+++ docs/html/search/all_1.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['calculate_5fcircle_5farea_0',['calculate_circle_area',['../namespacearea__calculator.html#a50f993edd0901b813f14ee29441c48de',1,'area_calculator']]],
+  ['calculate_5fparallelogram_5farea_1',['calculate_parallelogram_area',['../namespacearea__calculator.html#ab9b83139f5aba22dde6e5b486711c02b',1,'area_calculator']]],
+  ['calculate_5fpolygon_5farea_2',['calculate_polygon_area',['../namespacearea__calculator.html#a29506b26bfb76607e3278caed091539b',1,'area_calculator']]],
+  ['calculate_5frectangle_5farea_3',['calculate_rectangle_area',['../namespacearea__calculator.html#aeabc5c99b9e8e6f67ec18d948b08aeb3',1,'area_calculator']]],
+  ['calculate_5fsquare_5farea_4',['calculate_square_area',['../namespacearea__calculator.html#acd19369786876d042d57f4ecc9167ae0',1,'area_calculator']]],
+  ['calculate_5ftrapezoid_5farea_5',['calculate_trapezoid_area',['../namespacearea__calculator.html#a5ea12d38c13d1ae283e04b449c640d1d',1,'area_calculator']]],
+  ['calculate_5ftriangle_5farea_6',['calculate_triangle_area',['../namespacearea__calculator.html#a01bc59613eda4c7bc168ab95d2d1ca33',1,'area_calculator']]],
+  ['compare_5fareas_7',['compare_areas',['../namespacearea__calculator.html#aaaaadcefab0ca0f71d4c0934af484996',1,'area_calculator']]]
+];
---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "acm",
+  1: "a",
+  2: "cm"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "namespaces",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "Указатель",
+  1: "Пространства имен",
+  2: "Функции"
+};
+
